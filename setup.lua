fcbc31d78ce2f9031300d456c294069df580eb56
Juicy <juicy@pop-os.localdomain>
2023-09-19 00:44:10 -0400

    Removed the Packer install... Gotta do it manually now

--- setup.lua
+++ setup.lua
@@ -18,8 +18,6 @@ end
 local function fullsetup()
     mkdir()
     copy()
-    os.execute("cd && git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim")
 end
 
 function Menu()
